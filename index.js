ffc292d7890a3b589bc179b67e95d39baf9c566e
kyra qiu <kyraqiu@gmail.com>
2022-03-08 15:45:53 -0500

    commit tree 9ededd06e0095cb5e4e38063f6bb9899d4c439e1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+
+multiply (2,31);
+// multiply = num1*num2;
+
+let random = Math.floor(Math.random() > 0);
+
+let mod;
+let num3 = 32;
+let num4 = 8;
+
+mod = num3/num4;
+
+let max = Math.max (5,10,15,20) ;
